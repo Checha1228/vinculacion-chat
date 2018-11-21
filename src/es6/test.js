c60ef544cdff147645918e38187f192f4ab70ffb
Checha <ladycastdg@gmail.com>
2018-11-21 17:57:30 -0500

    prueba

---
+++ src/es6/test.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function($) {
+	jQuery("#enviarSubmit").click(function(event) {
+		event.preventDefault();
+		var margin = jQuery(".group-chat-input:first-child").css('margin-top');
+		var number= margin.replace('px','');
+		number = number - 170;
+		if(number >= -1620) {
+			jQuery(".group-chat-input:first-child").css('margin-top',number+'px');
+
+		}
+	});
+});
